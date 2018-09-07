737f421a9de43face5def196f5b0264d3bc7f95a
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-07 17:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ class BoardMember {
   veto() {
     return "No, I must disagree"
   }
+
+  approve() {
+    return "You can do that!"
+  }
+
+  doCharity() {
+    return "I like to help people/"
+  }
 }
